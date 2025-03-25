c360f3e41fa729fd9681bcbf28e2c2377ed7c981
Mohammad Kaif <kaifghalib123@gmail.com>
2025-03-26 02:44:16 +0530

    fixed opacity error

--- src/Component/pages/Index.tsx
+++ src/Component/pages/Index.tsx
@@ -70,7 +70,7 @@ const Home = () => {
   return (
     <div className="relative flex h-screen">
       {isLoadingNewChat && (
-        <div className="absolute inset-0 z-50 flex items-center justify-center bg-black bg-opacity-30">
+        <div className="absolute inset-0 z-50 flex items-center justify-center bg-black opacity-65">
           <Loader2 size={48} className="animate-spin text-white" />
         </div>
       )}
